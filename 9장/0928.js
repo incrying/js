3211c755230ead445803f40f4f8530d5c5ec3c33
incrying <incrying@duksung.ac.kr>
2023-06-27 23:00:21 +0900

    [0627] 9장 예제

---
+++ 9장/0928.js
@@ -0,0 +1,7 @@
+var str = '';
+
+//문자열의 길이를 참조한다
+var length = str && str.length;
+
+//문자열의 길이를 참조하지 못한다
+console.log(length); //''
---
+++ 9장/0929.js
@@ -0,0 +1,5 @@
+var str = '';
+
+//문자열의 길이를 참조한다.
+var length = str?.length;
+console.log(length); //0
